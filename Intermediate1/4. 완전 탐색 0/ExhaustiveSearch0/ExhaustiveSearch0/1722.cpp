086fab808d154fac8f2f31b0fb6de71809d2c49a
syshim77 <syshim77@gmail.com>
2019-04-10 17:19:44 +0900

    Intermediate 1/Exhaustive Search 0 study data

--- Intermediate1/4. 완전 탐색 0/ExhaustiveSearch0/ExhaustiveSearch0/1722.cpp
+++ Intermediate1/4. 완전 탐색 0/ExhaustiveSearch0/ExhaustiveSearch0/1722.cpp
@@ -1,3 +1,60 @@
+// 답 코드인데 이해가 안되는 부분이 있음
+// 다시 한 번 분석해볼 것
+#include <iostream>
+#include <vector>
+using namespace std;
+long long f[21] = { 1 };
+bool c[21];
+int main() {
+	for (int i = 1; i <= 20; i++) {
+		f[i] = f[i - 1] * i;
+	}
+	int n;
+	cin >> n;
+	int what;
+	cin >> what;
+	if (what == 2) {
+		vector<int> a(n);
+		for (int i = 0; i<n; i++) {
+			cin >> a[i];
+		}
+		long long ans = 0;
+		for (int i = 0; i<n; i++) {
+			for (int j = 1; j<a[i]; j++) {
+				if (c[j] == false) {
+					ans += f[n - i - 1];
+				}
+			}
+			c[a[i]] = true;
+		}
+		cout << ans + 1 << '\n';
+	}
+	else if (what == 1) {
+		long long k;
+		cin >> k;
+		vector<int> a(n);
+		for (int i = 0; i<n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (c[j] == true) continue;
+				if (f[n - i - 1] < k) {
+					k -= f[n - i - 1];
+				}
+				else {
+					a[i] = j;
+					c[j] = true;
+					break;
+				}
+			}
+		}
+		for (int i = 0; i<n; i++) {
+			cout << a[i] << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
+
+/* 시간 초과
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -54,3 +111,4 @@ int main() {
 	delete[] permut;
 	return 0;
 }
+*/
